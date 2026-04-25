96a51ea02c1d063ebc753eaff640d12ffc141a67
engosama102-ui <engosama102@gmail.com>
2026-04-25 19:11:56 +0300

    fix commands.js for PowerPoint

--- dist/commands.js
+++ dist/commands.js
@@ -1,2 +1,2 @@
-Office.onReady(function(){}),Office.actions.associate("action",function(e){var i={type:Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,message:"Performed action.",icon:"Icon.80x80",persistent:!0};Office.context.mailbox.item.notificationMessages.replaceAsync("ActionPerformanceNotification",i),e.completed()});
+Office.onReady(function(){}),Office.actions.associate("action",function(c){c.completed()});
 //# sourceMappingURL=commands.js.map
